cddcfb70a2693390232063ed3c63c1605d3c3854
Pleo2 <leooel23m@gmail.com>
2024-12-10 18:13:43 -0400

    add: TierListMaker

--- src/mock/projects.js
+++ src/mock/projects.js
@@ -2,6 +2,12 @@ export const projects = [
     {
         name: 'gyphy-clone-api-test',
         description:
+        'It is a tier list maker, with a easy change of theme and excelent performanst, it is build with react-beautifuld-dnd and use vite like compailer',
+        link: 'https://hacer-tier-list-vite.vercel.app/',
+    },
+    {
+        name: 'gyphy-clone-api-test',
+        description:
         'It is a clone of the official giphy page, feactures: optimization of the preview img in all sections, Mansory grid used in the official page. actually work in the mobile design.( only Desktop design ) ',
         link: 'https://gyphy-clone-api-test-eybqbk2wv-pleo2.vercel.app/',
     },
